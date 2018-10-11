abee94cd81955ca5b751c0cc800ec5dfe7152ae0
l-n-s <supervillain@riseup.net>
2018-10-11 04:30:28 -0400

    Add files

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # i2pvain.py
-Fancy I2P address generator
+
+i2pvain.py generates a customized [I2P](https://geti2p.net/) destination, which base32 address starts with a word you choose.
+
+For example, let's generate an address which starts with "cool" and save it to a file "cool.dat":
+
+    ./i2pvain.py cool cool.dat
+    cool672xmktk5ehyiccz2if76c6fhhhjdxxlfyftzhivnvhfkygq.b32.i2p
+    5F8keASxnf-AorD75fGkJD271L51Wz~U2yrw3MNHc8qIh50ffg0QVIcvocNhDQO-9t4nVOZ2l2oQ6tztgdhx4u~P1zeIqhLWZaQT0idVx4Iv-6WqZUH0htstUa3yOi12KGBB2NfkMPBZPb02Zo9Jwo6Sa7Xtnr57x8PXuoil-WScCSeBd6kXDB63oia7FmRCtVe4EaUhrjIyeK4Nb0eqlN2ELA0jcY4kMNLjIyWdJ8nkqlGwVXl0vdu3eaEudydbW99Jv-6qhGeaCTbzSZJfl4A2GK7vO5xuOFVAYY06sBLIv1PguG8vqIiHyEPtZZ3gnjWZQbewl8xiGoG0ymhT~d7P8V8TcQBmZlfpLVGlpHeUD2jC4DnZiMyXIf6Rq8-3n3gLK5oxmcYR9Xd9unt-bv0aNtWMsXmjgD~qOECkeVgs9OJHCaU5HYbf6foyHLW4-rtJsKwIGwUhliZg5ne3yaR6SE3DewDuCjJFkDU12xJENLfYB1LKyKQoaP8gF4ZkBQAEAAcAAKfpqPr03P4NGjsb1TQ4-iesxeyV6fIvTc-uEfcG8Ljh94k~irK-Cw4HRrNx-79s8-UtcJm2awnNVc2lfrSo-0XWIaMlsHTg~R~GGau2iRi0WYaG~fnpuOyURwf84ZMw3fxLbTUSYtGsS24eT1Q9lpcrgQHseq~6dBl2BWzXcL20rmOKU8n07AY7bO2E-2a0V9WD2kSJv~ecQ9lttaANJ1hsTIOXws056mDrXP3akXodH93kcZEq7WKH8ucbfqL1XGgFh24ZX7LRSXjE6FgfGBq9IMyWVTVxwvjSO2n6gbEuEyptzFF45tnvVDyTjli6pT6mjdvwCALxJuA0db1AJ29LmEMuQOMZagmmXJbKfrOqLu55f~AvSwQdFJdlcPlSow==
+    Key saved to --> cool.dat
+
+Now you can use "cool.dat" key file to create a new I2P tunnel, and its base32 address will be "cool672xmktk5ehyiccz2if76c6fhhhjdxxlfyftzhivnvhfkygq.b32.i2p"
+
+## requirements
+
+- Python 3.4+ 
+- I2P router with SAM API enabled
---
+++ i2pvain.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+import sys
+import os
+import struct
+import socket
+
+from hashlib import sha256
+from base64 import b32encode, b64decode, b64encode
+from multiprocessing import Process, Queue
+
+def get_sam_address():
+    value = os.getenv("I2P_SAM_ADDRESS")
+    if value:
+        value = value.split(":")
+        return (value[0], int(value[1]))
+    else:
+        return ("127.0.0.1", 7656)
+
+def vain(data, prefix, q):
+    cert_len = struct.unpack("!H", data[385:387])[0]
+
+    public_data = data[:387+cert_len]
+    data_tail = data[387+cert_len:]
+
+    head = public_data[:256] + os.urandom(88)
+    tail = public_data[352:]
+    head_hash = sha256(head)
+
+    while True:
+        padding = os.urandom(8)
+        new_hash = head_hash.copy() 
+        new_hash.update(padding + tail)
+        if b32encode(new_hash.digest()).startswith(prefix):
+            new_data = head + padding + tail
+            address = b32encode(new_hash.digest()).decode()[:52].lower()
+            break
+
+    q.put({"address": address, "data": new_data+data_tail})
+
+def get_new_destination(sam_address):
+    sam_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        sam_socket.connect(sam_address)
+    except ConnectionRefusedError:
+        print("SAM API is unavailable. Make sure SAM is enabled on your I2P router.")
+        exit()
+
+    sam_socket.send(b"HELLO VERSION MIN=3.1 MAX=3.1\n")
+    reply = sam_socket.recv(4096)
+    if reply == b"HELLO REPLY RESULT=OK VERSION=3.1\n":
+        sam_socket.send(b"DEST GENERATE SIGNATURE_TYPE=7\n")
+        reply = sam_socket.recv(4096)
+        dest = reply.split(b" ")[3][5:-1]
+        sam_socket.close()
+        return b64decode(dest, altchars="-~", validate=True)
+    else:
+        print(reply)
+        exit()
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: {} PREFIX [FILE]".format(sys.argv[0]))
+        exit()
+    else:
+        prefix = sys.argv[1].upper().encode()
+        if len(sys.argv) == 3:
+            outfile = sys.argv[2]
+        else:
+            outfile = "key.dat"
+
+    data = get_new_destination(get_sam_address())
+
+    processes = []
+    q = Queue()
+    for x in range(os.cpu_count()):
+        p = Process(target=vain, args=(data, prefix, q))
+        p.start()
+        processes.append(p)
+
+    new_key = q.get()
+    for x in processes: x.terminate()
+
+    print(new_key["address"] + ".b32.i2p")
+    print(b64encode(new_key["data"], altchars=b"-~").decode())
+
+    if os.access(os.path.dirname(outfile) or ".", os.W_OK):
+        with open(outfile, 'wb') as f: 
+            f.write(new_key["data"])
+
+        print("Key saved to -->", outfile)
+    else:
+        print(os.access(os.path.dirname(outfile), os.W_OK))
